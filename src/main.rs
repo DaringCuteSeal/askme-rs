e2d1dffd19053908b1babf2c1f7a35dfda1d8945
ezntek <eason@ezntek.com>
2023-06-03 22:43:51 +0800

    a lot of cleaning + nix file
    Co-authored-by: Pearl <daringcuteseal@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,68 @@
-use askme_memorize::App;
+use askme_memorize::{print_correct_answers, App, Settings};
 use clap::Parser;
-mod yaml_parser;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// AskMe file
-    #[arg()]
     filename: String,
+
+    #[arg(
+        default_value_t = 1.0,
+        long = "duration",
+        short = 'd',
+        help = "Choose how long the delay is between new questions (seconds, can be decimal)"
+    )]
+    wait_duration: f64,
+
+    #[arg(
+        long,
+        short = 'L',
+        help = "Choose if you would like to be quizzed in a loop"
+    )]
+    loop_questions: bool,
+
+    #[arg(long, short = 's', help = "Enable shuffling")]
+    shuffle: bool,
+
+    #[arg(
+        long,
+        short = 'C',
+        help = "Make answers to the questions case-sensitive"
+    )]
+    case_sensitive: bool,
+
+    #[arg(
+        long,
+        short = 'S',
+        help = "show the correct answer(s) if an answer was wrong"
+    )]
+    show_correct: bool,
+}
+
+impl From<Args> for Settings {
+    fn from(val: Args) -> Self {
+        Settings {
+            shuffle: val.shuffle,
+            loop_questions: val.loop_questions,
+            case_sensitive: val.case_sensitive,
+            show_correct: val.show_correct,
+            wait_duration: val.wait_duration,
+        }
+    }
 }
 
 fn main() {
     let args = Args::parse();
-    let mut askme_content = yaml_parser::parse_file(&args.filename);
-    let mut rng = rand::thread_rng();
-    let mut app = App {
-        askme_content: &mut askme_content,
-        q_index: 0,
-        correct_count: 0,
-        rng: &mut rng,
+
+    let mut app = match App::from_file(&args.filename.clone(), args.into()) {
+        Ok(a) => a,
+        Err(e) => panic!("error: {}", e),
     };
 
-    app.main_loop();
+    let correct_count = match app.run() {
+        Ok(c) => c,
+        Err(e) => panic!("error: {}", e),
+    };
+
+    print_correct_answers(correct_count, app.askme_file.questions.len());
 }
